eb91a4bff85b5c8598fc70839dc4aec19ce4f7a1
LizVelasco <lizt.velasco@gmail.com>
2023-10-26 14:54:13 -0500

    Dinamica de seguir de los animales

---
+++ Assets/Scripts/AnimalNavigation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimalNavigation : MonoBehaviour
+{
+    private bool canFollow = false;
+    public Transform player;
+    protected NavMeshAgent agentAnimal;
+    void Start()
+    {
+        agentAnimal = GetComponent<NavMeshAgent>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canFollow = true;
+        }
+    }
+    private void Update()
+    {
+        if (canFollow)
+        {
+            agentAnimal.SetDestination(player.position);
+        }
+    }
+
+
+
+}
