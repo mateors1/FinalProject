2c7d4eae6f60383ecd1da49b8f7378cc7d75518e
Mateo Rendon <mateorendon1@gmail.com>
2023-10-30 15:27:21 -0500

    fixes i hope

--- Assets/FirstPartyAssets/Scripts/Gameplay/CrocBridge.cs
+++ Assets/FirstPartyAssets/Scripts/Gameplay/CrocBridge.cs
@@ -6,17 +6,50 @@ using UnityEngine.AI;
 
 public class CrocBridge : MonoBehaviour
 {
-    NavMeshLink crocBridge;    // Start is called before the first frame update
+    NavMeshLink crocBridge;
+    [SerializeField] int requiredCrocs;
+    [SerializeField] int currentCrocs;
+    [SerializeField] GameObject[] crocs;
+    int crocIndex;
+    // Start is called before the first frame update
     void Start()
     {
         crocBridge = GetComponent<NavMeshLink>();
 
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Croc"))
+        {
+            other.gameObject.SetActive(false);
+            AddCroc();
+        }
+            
+           
+
         
     }
+
+    private void AddCroc()
+    {
+        crocs[crocIndex].SetActive(true);
+        crocIndex++;
+        if (crocIndex >= crocs.Length - 1)
+        {
+            crocIndex = crocs.Length - 1;
+        }
+        currentCrocs++;
+        if (currentCrocs == requiredCrocs)
+        {
+            crocBridge.enabled = true;
+        }
+
+    }
+    
+
+
+
 }
