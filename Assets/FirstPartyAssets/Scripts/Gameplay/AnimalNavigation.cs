ad996dbf67f72b8f21dd283d178f518036a58197
LizVelasco <lizt.velasco@gmail.com>
2023-11-05 13:54:50 -0500

    contador cocodrilos

--- Assets/FirstPartyAssets/Scripts/Gameplay/AnimalNavigation.cs
+++ Assets/FirstPartyAssets/Scripts/Gameplay/AnimalNavigation.cs
@@ -25,14 +25,10 @@ public class AnimalNavigation : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            CounterTrigger.Instance.ChangeAnimalSprite(gameObject.tag);
-        }
 
         if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.Q))
         {
-        
+            CounterTrigger.Instance.ChangeAnimalSprite(gameObject.tag);
             HelpMeMeow();
             animator.SetBool("IsWalking", true);
 
