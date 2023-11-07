7392e7f12a969f76e5bbf9c843718718149c2324
Luna Duran <lunaduranp@gmail.com>
2023-11-07 14:49:08 -0500

    ApplePrefab sound

--- Assets/FirstPartyAssets/Scripts/Gameplay/Apple.cs
+++ Assets/FirstPartyAssets/Scripts/Gameplay/Apple.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    public AudioClip pickupSound;
+    private AudioSource audioSource; 
+
+    private void OnEnable()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         AppleInventory inventory = collision.gameObject.GetComponent<AppleInventory>();
 
-        if (inventory != null )
+        if (inventory != null)
         {
+            audioSource.PlayOneShot(pickupSound);
             inventory.ApplePickUp();
             gameObject.SetActive(false);
+
+            
         }
     }
 }
